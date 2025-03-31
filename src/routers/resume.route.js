1cc87d84cef661a7bf92c861715960c9d1d083c7
AkASH jain <aj9882854@gmail.com>
2025-04-01 01:41:11 +0530

    resume

--- src/routers/resume.route.js
+++ src/routers/resume.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { uploadresume , analizeresume, createresume  } from '../controllers/resume.controller.js';
+import { uploadresume , analizeresume, createresume  } from '../controllers/Resume.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyjwt } from '../middlewares/auth.middleware.js';
 const router = Router();
